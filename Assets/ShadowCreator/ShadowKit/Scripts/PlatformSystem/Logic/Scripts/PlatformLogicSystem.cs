27d83888cccd43af9449da741cfa7e628d5c9540
wang <wang>
2019-04-18 17:52:11 +0800

    目录整理

--- Assets/ShadowCreator/ShadowKit/Scripts/PlatformSystem/Logic/Scripts/PlatformLogicSystem.cs
+++ Assets/ShadowCreator/ShadowKit/Scripts/PlatformSystem/Logic/Scripts/PlatformLogicSystem.cs
@@ -7,19 +7,8 @@ using UnityEngine.Networking;
 
 
 public class PlatformLogicSystem : MonoBehaviour {
-
-    public const string LOGTAG = "[PlatformSystem]:";
-
-    public static PlatformLogicSystem Instant;
     
-    /// <summary>
-    /// 从AndroidMainfest获取
-    /// </summary>
-    private string scAppId = "xxxxx";
-
-    private static float platformTaskInterval = 5;
-
-    private float timeCount = platformTaskInterval;
+    public static PlatformLogicSystem Instant;
 
     void Awake() {
         if (Instant != null) {
@@ -31,52 +20,12 @@ public class PlatformLogicSystem : MonoBehaviour {
     }
 
     void Start() {
-        //WWWForm form = new WWWForm();
-        //form.AddField("loginstr", "18918690343");
-        //form.AddField("password", "wang89c51");
-        //WebRequestBase wq = new WebRequestLogin(form);
-        //WebRequestManager.Instant.AddWebRequest(wq);
-
-        //LoginSystem.SetStrategy(new LoginStrategyAccountAndPasswordFrowWebRequest());
-        //LoginSystem.Login();
+        DebugMy.Log("Start",this);
         VerifyStatusSystem.Instant.SetVerifyStrategy(new VerifyStrategy1());
     }
 
     void Update() {
         VerifyStatusSystem.Instant.OnUpdate();
-        
-
-        //return;
-        //if (SvrManager.Instance.status.running) {
-        //    if ( (timeCount += Time.deltaTime) > platformTaskInterval) {
-        //        timeCount = 0;
-        //        Debug.Log(LOGTAG+"Do PlatformTasks");
-        //        PlatformTasks();
-        //    }
-        //}
     }
-
-    //void PlatformTasks() {
-
-    //    if (AppInfo.isAPPLegal == false) {
-
-    //        VerifyAPP.Start();
-
-    //    }else if (AppInfo.isAPPLegal == true) {
-
-    //        VerifySystemToken.Start();
-
-    //        if (SysInfo.isTokenLegal == true) {
-
-    //            VerifyAPPToken.Start();
-
-    //        }
-
-    //    }
-    //}
-    
-
-    
-
 
 }
